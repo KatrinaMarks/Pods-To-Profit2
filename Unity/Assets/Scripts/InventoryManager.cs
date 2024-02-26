9cdad1cd1c7d82225ce7fb541e876cafc3480ea1
HaydenParsons <haydenparkerparsons@gmail.com>
2024-02-26 13:42:10 -0500

    started working on inventory array and function

--- Unity/Assets/Scripts/InventoryManager.cs
+++ Unity/Assets/Scripts/InventoryManager.cs
@@ -20,6 +20,26 @@ public class InventoryManager : MonoBehaviour
     public TMP_Text pText;
     public TMP_Text fText;
 
+    /* 2D array for the inventory, first index is the type of item (seed, fertilizer, etc)
+     * and the second index is the status (conventional, sustainable, organic). I plan on 
+     * making ENUMs for both of these so we don't have to memorize which number corresponds to
+     * which item or status
+     */
+
+    public enum items {
+      RHIZO; // rhoizobium
+      BIOPEST; // bio pesticides
+      FERT; // fertilzier 
+      SEED; // seeds
+    }
+
+    public enum status {
+      CON;  // conventional
+      SUS; // sustainable
+      ORG; // organic
+    }
+    public int[][] inventory;
+
     public bool ownTractor = false;
     public bool brokenTractor = false;
     // Start is called before the first frame update
@@ -28,6 +48,15 @@ public class InventoryManager : MonoBehaviour
       moneyText.text = "$" + money;
       shopMoneyText.text = "$" + money;
       // moneyTextDisplay.text = "$" + money.ToString();
+
+      /* Do I even need this? Just initializing the inventory array, but it doesn't
+       * seem like the old group ever fully initialized the menu...[] arrays in TurnManager
+       */
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+          inventory[i][j] = 0;
+        }
+      }
     }
 
     // Update is called once per frame
@@ -56,5 +85,21 @@ public class InventoryManager : MonoBehaviour
         return true;
       }
       return false;
+    }
+
+    /* Adds and subtracts from the inventory -- positive amount to add, negative to subtract
+     * type and status parameters so that we only need one function for every button/action
+     * that interacts with the inventory. 
+     */
+    public bool changeInventory(int type, int status, int amount) {
+      /* If negative amount, also need to have enough in inventory, or if positive amount,
+       * then just change inventory as needed and return true, else return false
+       */
+      if ((amount < 0 && inventory[type][status] > 0) || amount > 0) {
+        inventory[type][status] += amount;
+        return true;
+      } else {
+        return false;
+      }
     }
 }
