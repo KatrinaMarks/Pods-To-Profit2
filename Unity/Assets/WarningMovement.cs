efc61c5fc0513ea3d9c2117bcfdabaad3990623e
KatrinaMarks <curlyhurricane@gmail.com>
2024-02-27 22:25:27 -0500

    Working on warning prefab

--- Unity/Assets/WarningMovement.cs
+++ Unity/Assets/WarningMovement.cs
@@ -8,7 +8,9 @@ public class WarningMovement : MonoBehaviour
     public Shaker Shaker;
     public float durationToSet = 0.3f;
     Transform target;
+    Transform target2;
     Vector3 initPos;
+    Vector3 initPos2;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,10 @@ public class WarningMovement : MonoBehaviour
         target.localPosition = slideLeft;
 
         WarningBlock.SetActive(true);
+        target2 = WarningBlock.GetComponent<Transform>();
+        initPos2 = target2.localPosition;
+        var slideRight = new Vector3(225f, initPos2.y, initPos2.y);
+        target2.localPosition = slideRight;
     }
 
     // Update is called once per frame
