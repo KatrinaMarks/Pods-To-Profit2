e64478a326a397ed31f5ab2ee7b98de46d896b7e
KatrinaMarks <curlyhurricane@gmail.com>
2024-02-27 22:55:41 -0500

    added sliding to warning prefab

--- Unity/Assets/WarningMovement.cs
+++ Unity/Assets/WarningMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WarningMovement : MonoBehaviour
 {
+    public GameObject WarningHexagon;
     public GameObject WarningBlock;
     public Shaker Shaker;
     public float durationToSet = 0.3f;
@@ -11,27 +12,30 @@ public class WarningMovement : MonoBehaviour
     Transform target2;
     Vector3 initPos;
     Vector3 initPos2;
+    public float moveSpeed = 500;
     // Start is called before the first frame update
     void Start()
     {
+        // Call the shake function to shake the warning hexagon
         Shaker.Shake(durationToSet);
 
-        target = GetComponent<Transform>();
-        initPos = target.localPosition;
-        
-        var slideLeft = new Vector3(-225f, initPos.y, initPos.z);
-        target.localPosition = slideLeft;
-
         WarningBlock.SetActive(true);
-        target2 = WarningBlock.GetComponent<Transform>();
-        initPos2 = target2.localPosition;
-        var slideRight = new Vector3(225f, initPos2.y, initPos2.y);
-        target2.localPosition = slideRight;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Slide the hexagon to the left until it reaches desired position
+        if (WarningHexagon.transform.localPosition.x > -225) {
+            WarningHexagon.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }    
+
+        // Slide the warning message to the right until it reached desired position
+        if (WarningBlock.transform.localPosition.x < 200) {
+            WarningBlock.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }    
+            
         
     }
 }
